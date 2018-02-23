fbf892aa81132a39dc878e133d9401bd0ef45d6d
Antoine Gaillot <manzagregine@gmail.com>
2018-02-23 12:16:37 +0100

    Adding eslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    'extends': 'airbnb-base',
+    'rules': {
+        'no-underscore-dangle': 'off',
+        'indent': [0, 4],
+        'comma-dangle': 0,
+        'import/export': 0,
+        'object-curly-spacing': 0,
+        'no-trailing-spaces': 0,
+        'quotes': [
+            0,
+            'single',
+        ],
+        'semi': ['error', 'never'],
+    },
+}
